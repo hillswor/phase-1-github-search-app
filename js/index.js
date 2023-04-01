5dcd927ad731986b025150a97fee6ca91fe6e51d
Bruce <bruce.hillsworth@gmail.com>
2023-03-31 20:39:53 -0600

    Added fetch to github api to display list of users based upon search

---
+++ js/index.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const searchForm = document.querySelector("#github-form");
+	const userList = document.querySelector("#user-list");
+
+	searchForm.addEventListener("submit", (event) => {
+		event.preventDefault();
+		fetch(
+			`https://api.github.com/search/users?q=${event.target.search.value}`
+		)
+			.then((response) => response.json())
+			.then((userData) => {
+				userData.items.forEach((user) => {
+					const li = document.createElement("li");
+					li.textContent = user.login;
+					userList.appendChild(li);
+				});
+			});
+		searchForm.reset();
+	});
+});
